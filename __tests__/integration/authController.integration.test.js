ebfb6eb45ce916db0ca2e570106db91ce4fbb094
mahadhir247 <mahadhir123456@gmail.com>
2026-03-17 13:59:17 +0800

    Add integration testing for backend registration
    Added integration testing for backend registration components.
    
    Acceptance criteria fulfilled:
    - User should register successfully with valid data
    - User should receive error message if register using existing email
    - User should receive error message if there is an exception

---
+++ __tests__/integration/authController.integration.test.js
@@ -0,0 +1,142 @@
+/* Name: Mahadhir Bin Mohd Ismail
+ * Student No: A0252808B
+ */
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import userModel from "../../models/userModel.js";
+import { registerController } from "../../controllers/authController.js";
+
+describe("Integration tests for registration components", () => {
+    let mongoServer;
+    let mock_user0;
+    let req, res;
+
+    beforeAll(async () => {
+        // Set up mongo server for testing
+        mongoServer = await MongoMemoryServer.create();
+        const uri = mongoServer.getUri();
+        await mongoose.connect(uri);
+
+        // Silence console log
+        jest.spyOn(global.console, 'log').mockImplementation(() => {});
+    });
+
+    beforeEach(async () => {
+        mock_user0 = await new userModel({
+            name: 'Existing User',
+            email: 'existuser@gmail.com',
+            phone: '1234567890',
+            address: 'existaddr',
+            password: 'existhashedpassword',
+            answer: 'existanswer',
+        }).save();
+
+        req = {
+            body: {}
+        };
+        res = {
+            send: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+    });
+
+    afterEach(async () => {
+        await userModel.deleteMany({});
+        jest.clearAllMocks();
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongoServer.stop();
+
+        jest.restoreAllMocks();
+    });
+
+    describe("Integration between registerController and userModel", () => {
+        it("should return error if user already exists", async () => {
+            req.body = {
+                name: 'Existing User',
+                email: 'existuser@gmail.com',
+                phone: '1234567890',
+                address: 'existaddr',
+                password: 'existhashedpassword',
+                answer: 'existanswer',
+            };
+
+            await registerController(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({
+                success: false,
+                message: "User is already registered. Please login.",
+            });
+        });
+
+        it("should return 500 if database has errors", async () => {
+            await mongoose.disconnect();
+            req.body = {
+                name: 'New User',
+                email: 'newuser@gmail.com',
+                phone: '1234567890',
+                address: 'newaddr',
+                password: 'newpassword',
+                answer: 'newanswer',
+            };
+
+            await registerController(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    success: false,
+                    message: 'Error in Registration',
+                })
+            );
+
+            // Reset db connection
+            await mongoose.connect(mongoServer.getUri());
+        });
+    });
+
+    describe("Integration between registerController, userModel and hashPassword", () => {
+        it("should register user successfully", async () => {
+            req.body = {
+                name: 'New User',
+                email: 'newuser@gmail.com',
+                phone: '1234567890',
+                address: 'newaddr',
+                password: 'newpassword',
+                answer: 'newanswer',
+            };
+
+            await registerController(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.send).toHaveBeenCalledWith({
+                success: true,
+                message: "User registered successfully",
+                user: expect.objectContaining({
+                    name: 'New User',
+                    email: 'newuser@gmail.com',
+                    phone: '1234567890',
+                    address: 'newaddr',
+                    answer: 'newanswer',
+                    role: 0,
+                }),
+            });
+
+            // Verify if db saves user
+            const savedUser = await userModel.findOne({ email: 'newuser@gmail.com' });
+            expect(savedUser).toMatchObject({
+                name: 'New User',
+                email: 'newuser@gmail.com',
+                phone: '1234567890',
+                address: 'newaddr',
+                answer: 'newanswer',
+                role: 0,
+            });
+            expect(savedUser.password).not.toBe('newpassword'); // Password is hashed
+        });
+    });
+});
