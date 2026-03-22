bcdab31a5eaa58b9523f2ee93e1a38f2d794c564
qinxutan <122926007+qinxutan@users.noreply.github.com>
2026-03-23 00:24:00 +0800

    dup

--- controllers/testController.js
+++ controllers/testController.js
@@ -52,8 +52,6 @@ export const seedDatabase = async (req, res) => {
     console.log('✅ Categories created:', Object.keys(categoryMap).length);
 
     const productMap = {};
-    // Create products, resolving categorySlug --> _id and loading images from disk
-    const productMap = {};
     for (const product of TEST_PRODUCTS) {
       const { categorySlug, photoFilename, contentType, ...rest } = product;
       
