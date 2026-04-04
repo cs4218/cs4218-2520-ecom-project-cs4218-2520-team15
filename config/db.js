f959222e250063982b8aa2e3714b21fcdda25e15
miljyy <miljy123@gmail.com>
2026-04-04 21:41:57 +0800

    Update config/db.js to resolve potential merge conflicts

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
-import colors from "colors";
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white);
+        const url = 
+            process.env.USE_TEST_DB === "true" && process.env.MONGO_TEST_URL
+                ? process.env.MONGO_TEST_URL
+                : process.env.MONGO_URL;
+        const conn = await mongoose.connect(url);
+        console.log(
+            `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
+        );
     } catch (error) {
         console.log(`Error in Mongodb ${error}`.bgRed.white);
     }
